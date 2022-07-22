a064cfaf74949ad3718863a3a50d95e53a66ee7b
Pavan-A84 <109798904+Pavan-A84@users.noreply.github.com>
2022-07-22 16:18:36 +0530

    1st demo

---
+++ dhana.java
@@ -0,0 +1,22 @@
+package Practice;
+
+public class dhana {
+
+	public static void main(String[] args) {
+   double salary =25000;
+   int exp=7;
+   if(exp>5)
+   {
+	   double div=0.15;
+	   double bonus=div*salary;
+	   salary=salary+bonus;
+	  
+		   
+	   }
+   System.out.println("total salary is"+salary);
+   
+   }
+}
+
+   
+   
